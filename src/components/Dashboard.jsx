7f7294f83e5c6a711429a702a300df35b21578bb
Breno Barbosa <102376286+BrenoRodBar@users.noreply.github.com>
2023-12-29 17:43:16 -0300

    Primeiro Commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Card from './Card';
+Card()
+const Dashboard = () => {
+  return (
+    <div className='h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3'></div>
+  )
+}
+
+export default Dashboard;
