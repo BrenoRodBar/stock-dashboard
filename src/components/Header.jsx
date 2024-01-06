a1bd050f53cb1158ef2f8d790a3297c783e6c033
Breno Barbosa <102376286+BrenoRodBar@users.noreply.github.com>
2024-01-06 13:11:03 -0300

    Segundo commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { mockCompanyDetails } from '../constants/mock'
 import Search from './Search'
+import ThemeIcon from './ThemeIcon'
 
 const Header = ({ name }) => {
   return <>
@@ -8,7 +9,7 @@ const Header = ({ name }) => {
       <h1 className='text-5xl'>{name}</h1>
       <Search/>
     </div>
-    {/* {<ThemeIcon/>} */}
+    <ThemeIcon />
   </>
 }
 
