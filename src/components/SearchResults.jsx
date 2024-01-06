a9b7c1a0465c530aee3c85dbe4dd0c9e869822a7
Breno Barbosa <brb95_@hotmail.com>
2024-01-06 13:53:10 -0300

    Quarto commit

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 
 const SearchResults = ({results}) => {
   return (
-  <ul className='absolute top-12 border-2 w-full rounded-md h-64 overflow-y-scroll bg-white border-neutral-200 custom-scrollbar'>
+  <ul className={`absolute top-12 border-2 w-full rounded-md h-64 overflow-y-scroll  ${darkMode ? "bg-gray-900 border-gray-800 custom-scrollbar-dark" : "bg-white border-neutral-200 custom-scrollbar"}`}>
     {results.map((item) => {
-      return <li key={item.symbol} className='flex cursor-pointer p-4 m-2 item-center justify-between rounded-md hover:bg-indigo-200'>
-        <span>{item.symbol}</span>
-        <span>{item.description}</span>
-      </li>
+      return (
+        <li
+          key={item.symbol}
+          className={`flex cursor-pointer p-4 m-2 item-center justify-between rounded-md hover:bg-indigo-200 ${darkMode ? "hover:bg-indigo-600": "hover::bg-indigo-200"}`}
+        >
+          <span>{item.symbol}</span>
+          <span>{item.description}</span>
+        </li>
+      );
     })}
   </ul>
   );
